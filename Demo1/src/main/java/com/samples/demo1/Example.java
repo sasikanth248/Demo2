69865d4ad5a1291eb299b96cd7ba7b2f1f4893c6
sasikanth <sasikanth@krishtipati>
2018-01-15 15:04:25 +0530

    Demo Purpose

---
+++ Demo1/src/main/java/com/samples/demo1/Example.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.samples.demo1;
+
+/**
+ * @author sasikanth
+ *
+ */
+public class Example {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("This is example 1");
+	}
+
+}
